835fb87cbec3e082bca4d1eb0ee6b613b891cadc
Mauricio Silveira <mauricio@livreti.com.br>
2023-06-15 12:43:30 -0300

    v7.4.0 first release
    Signed-off-by: Mauricio Silveira <mauricio@livreti.com.br>

--- scripts/base.py
+++ scripts/base.py
@@ -515,8 +515,12 @@ def get_branding_repositories(checker):
 def create_pull_request(branches_to, repo, is_no_errors=False, is_current_dir=False):
   print("[git] create pull request: " + repo)
   url = "https://github.com/ONLYOFFICE/" + repo + ".git"
+  if (repo == "server"):
+    url = "https://github.com/msilveirabr/" + repo + ".git"
   if config.option("git-protocol") == "ssh":
     url = "git@github.com:ONLYOFFICE/" + repo + ".git"
+    if (repo == "server"):
+      url = "git@github.com:msilveirabr/" + repo + ".git"
   folder = get_script_dir() + "/../../" + repo
   if is_current_dir:
     folder = repo
