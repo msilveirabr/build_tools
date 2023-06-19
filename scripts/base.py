0aef92cecbc51f94162f6dc4b1102b92bd11a275
Mauricio Silveira <mauricio@livreti.com.br>
2023-06-19 19:23:38 -0300

    New tool fix 3@20230619-192300
    Signed-off-by: Mauricio Silveira <mauricio@livreti.com.br>

--- scripts/base.py
+++ scripts/base.py
@@ -441,9 +441,9 @@ def set_cwd(dir):
   return
 
 # git ---------------------------------------------------
-def git_update(repo, is_no_errors=False, is_current_dir=False, git_owner="msilveirabr"):
+def git_update(repo, is_no_errors=False, is_current_dir=False, git_owner=""):
+  print("[git] update: " + repo + " (owner=" + owner + ")")
   ltirepo = [ "server", "web-apps" ]
-  print("[git] update: " + repo)
   owner = "msilveirabr" if any(repo in s for s in ltirepo) else git_owner
   owner = git_owner if git_owner else "ONLYOFFICE"
   url = "https://github.com/" + owner + "/" + repo + ".git"
